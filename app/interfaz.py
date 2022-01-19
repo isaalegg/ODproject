65e8cd3fe1f1ccd2bfcb76cf297e63317c4cc005
isaalegg <isabel@mammut.io>
2022-01-19 18:45:53 -0400

    use case interface

--- app/interfaz.py
+++ app/interfaz.py
@@ -10,27 +10,13 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 
-
 st.title('Revelio Charm')
-
-
-losed_thing = st.radio('tell us what you lost', ['None', 'phone'])
-directory = os.path.dirname(os.path.realpath(__file__))
-path = os.path.join(directory, 'dataset', losed_thing)
-if not losed_thing == 'None':
-    train_data = os.path.join(path, "train")
-    val_data = os.path.join(path, "val")
-    st.write('calm down, we will find it')
-elif losed_thing == 'None':
-    st.write('we can not help you if you do not tell us what you lost, please.')
 
 
 def get_training_path(pathh):
     t_path = os.path.join(pathh, "train", "images")
     v_path = os.path.join(pathh, "val", "images")
     return t_path, v_path
-
-train_path, val_path = get_training_path(path)
 
 
 def get_revelio_results(image, colors, model):
@@ -71,22 +57,30 @@ def get_revelio_results(image, colors, model):
     plt.savefig('result.png')
 
 
+losed_thing = st.radio('tell us what you lost', ['None', 'phone'])
+directory = os.path.dirname(os.path.realpath(__file__))
+
+if not losed_thing == 'None':
+    path = os.path.join(directory, 'dataset', losed_thing)
+    st.write(f'calm down, we will find your {losed_thing}')
+if losed_thing == 'None':
+    st.write('we can not help you if you do not tell us what you lost, please.')
+
+train_path, val_path = get_training_path(path)
+
 COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
           [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
 
-label = 'train model'
-if st.button(label):
-    trainer = ObjectDetectionTrainer('Detr', train_path, val_path, directory)
-
-file = st.file_uploader(f"give us a image")
-if file:
-    im = Image.open(file)
-    magic = trainer.model.model
-    get_revelio_results(im, COLORS, magic)
-    st.image('result.png')
-
-@st.cache
-def get_model():
-    model_class = ObjectDetectionTrainer('Detr', train_path, val_path, directory)
-    model_instance = model_class.model
-    return model_instance
+label = f'find your {losed_thing}'
+if 'key' not in st.session_state:
+    click_there = st.button(label)
+    if click_there:
+        model_class = ObjectDetectionTrainer('Detr', train_path, val_path, directory, start=False)
+        st.session_state.key = model_class.model.model
+else:
+    file = st.file_uploader(f"what is the last place where you saw it?")
+    if file:
+        im = Image.open(file)
+        magic = st.session_state.key
+        get_revelio_results(im, COLORS, magic)
+        st.image('result.png')
